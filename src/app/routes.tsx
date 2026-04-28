5f07d0e434458ad24b85960ec55550e4a362c65c
Milena Soares <mimiisoares2003@gmail.com>
2026-04-28 20:22:00 -0300

    fix: possibilita rodar página

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,13 +3,18 @@ import { Layout } from "./components/Layout";
 import { Homepage } from "./components/Homepage";
 import { Project } from "./components/Project";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Homepage /> },
+        { path: "project", element: <Project /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <Homepage /> },
-      { path: "project", element: <Project /> },
-    ],
-  },
-]);
+    basename: "/NicoleCarneiro",
+  }
+);
